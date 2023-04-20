1976fe3ce3c8c2d3eb65b62575c53062d16d0af6
Victor Benjamin <129224408+Benjamin17B@users.noreply.github.com>
2023-04-20 15:21:07 -0600

    AÑADIENDO VALIDACION DE LOS DATOS

--- js/validar.js
+++ js/validar.js
@@ -8,11 +8,25 @@ function carga(){
   document.getElementById('NUsuario').addEventListener('input',VUsuario,false);
   document.getElementById('APMaterno').addEventListener('input',VapeM,false);
   document.getElementById('APParteno').addEventListener('input',VapeP,false);
+  document.getElementById('Marca').addEventListener('input',Vmarca,false);
+  document.getElementById('Modelo').addEventListener('input',Vmodelo,false);
+  document.getElementById('numSerie').addEventListener('input',Vnumserie,false);
+  document.getElementById('numInventario').addEventListener('input',Vinventario,false);
+  document.getElementById('So').addEventListener('input',Vso,false);
+  document.getElementById('Procesador').addEventListener('input',Vproce,false);
+  document.getElementById('DiscoDuro').addEventListener('input',Vdiscduro,false);
+  document.getElementById('Ram').addEventListener('input',Vram,false);
+  document.getElementById('TipoMemoria').addEventListener('input',Vtpmemoria,false);
+  document.getElementById('contraseña').addEventListener('input',Vcontra,false);
+  document.getElementById('NEquipo').addEventListener('input',VnEquipo,false);
+  document.getElementById('IP').addEventListener('input',VIP,false);
+  document.getElementById('Mac').addEventListener('input',VMac,false);
 
   document.getElementById('btnGuardar').addEventListener('click',vENVIAR,false);
 }
 function vENVIAR(e){
-  if(VUsuario() && VapeM() && VapeP() && confirm("Pulsa Aceptar si deseas Enviar el formulario")){
+  if(VUsuario() && VapeM() && VapeP() && Vmarca() && Vmodelo() && Vnumserie() && Vinventario()
+   && Vproce() && Vdiscduro() && Vram() && Vtpmemoria() && Vcontra() && VnEquipo() && VIP() && VMac() && confirm("Pulsa Aceptar si deseas Enviar el formulario")){
     formulario.submit();
     return true;
   } else {
@@ -26,7 +40,6 @@ function muestra(elemento,mensaje){
   error.className = "error active";
 }
 
-//Validar Nombre
 function VUsuario(){
   var elemento = document.getElementById('NUsuario');
   error = document.querySelector("#NUsuario + span.error");
@@ -40,10 +53,13 @@ function VUsuario(){
     if(elemento.validity.valueMissing){
       muestra(elemento,"Ingrese el Nombre");
     }
+    if(elemento.validity.patternMismatch){
+      muestra(elemento,"Solo Acepta Letras Mayusculas");
+    }
     return false;
   }
 }
-//Validar Apellido Paterno
+
 function VapeP(){
   var elemento = document.getElementById('APParteno');
   error = document.querySelector("#APParteno + span.error");
@@ -57,10 +73,13 @@ function VapeP(){
     if(elemento.validity.valueMissing){
       muestra(elemento,"Ingrese el apellido Paterno");
     }
+    if(elemento.validity.patternMismatch){
+      muestra(elemento,"Solo Acepta Letras Mayusculas");
+    }
     return false;
   }
 }
-//Validar Apellido Materno
+
 function VapeM(){
   var elemento = document.getElementById('APMaterno');
   error = document.querySelector("#APMaterno + span.error");
@@ -73,6 +92,246 @@ function VapeM(){
   else{
     if(elemento.validity.valueMissing){
       muestra(elemento,"Ingrese el Apellido Materno");
+    }
+    if(elemento.validity.patternMismatch){
+      muestra(elemento,"Solo Acepta Letras Mayusculas");
+    }
+    return false;
+  }
+}
+
+function Vmarca(){
+  var elemento = document.getElementById('Marca');
+  error = document.querySelector("#Marca + span.error");
+
+  if(elemento.checkValidity()){
+    error.innerHTML = "";
+    error.className = "error";
+    return true;
+  }
+  else{
+    if(elemento.validity.valueMissing){
+      muestra(elemento,"Ingrese La marca");
+    }
+    if(elemento.validity.patternMismatch){
+      muestra(elemento,"Solo Acepta Letras Mayusculas");
+    }
+    return false;
+  }
+}
+
+function Vmodelo(){
+  var elemento = document.getElementById('Modelo');
+  error = document.querySelector("#Modelo + span.error");
+
+  if(elemento.checkValidity()){
+    error.innerHTML = "";
+    error.className = "error";
+    return true;
+  }
+  else{
+    if(elemento.validity.valueMissing){
+      muestra(elemento,"Ingrese el Modelo");
+    }
+    if(elemento.validity.patternMismatch){
+      muestra(elemento,"Solo Acepta Letras Mayusculas");
+    }
+    return false;
+  }
+}
+
+function Vnumserie(){
+  var elemento = document.getElementById('numSerie');
+  error = document.querySelector("#numSerie + span.error");
+
+  if(elemento.checkValidity()){
+    error.innerHTML = "";
+    error.className = "error";
+    return true;
+  }
+  else{
+    if(elemento.validity.valueMissing){
+      muestra(elemento,"Ingrese el Numero de Serie");
+    }
+    return false;
+  }
+}
+
+function Vinventario(){
+  var elemento = document.getElementById('numInventario');
+  error = document.querySelector("#numInventario + span.error");
+
+  if(elemento.checkValidity()){
+    error.innerHTML = "";
+    error.className = "error";
+    return true;
+  }
+  else{
+    if(elemento.validity.valueMissing){
+      muestra(elemento,"Ingrese el Numero de Inventario");
+    }
+    if(elemento.validity.patternMismatch){
+      muestra(elemento,"Solo Acepta Letras Mayusculas");
+    }
+    return false;
+  }
+}
+
+function Vso(){
+  var elemento = document.getElementById('So');
+  error = document.querySelector("#So + span.error");
+
+  if(elemento.checkValidity()){
+    error.innerHTML = "";
+    error.className = "error";
+    return true;
+  }
+  else{
+    if(elemento.validity.valueMissing){
+      muestra(elemento,"Ingrese el SO");
+    }
+    return false;
+  }
+}
+
+function Vproce(){
+  var elemento = document.getElementById('Procesador');
+  error = document.querySelector("#Procesador + span.error");
+
+  if(elemento.checkValidity()){
+    error.innerHTML = "";
+    error.className = "error";
+    return true;
+  }
+  else{
+    if(elemento.validity.valueMissing){
+      muestra(elemento,"Ingrese el Procesador");
+    }
+    return false;
+  }
+}
+
+function Vdiscduro(){
+  var elemento = document.getElementById('DiscoDuro');
+  error = document.querySelector("#DiscoDuro + span.error");
+
+  if(elemento.checkValidity()){
+    error.innerHTML = "";
+    error.className = "error";
+    return true;
+  }
+  else{
+    if(elemento.validity.valueMissing){
+      muestra(elemento,"Ingrese el Disco Duro");
+    }
+    if(elemento.validity.patternMismatch){
+      muestra(elemento,"Solo Acepta Letras Mayusculas");
+    }
+    return false;
+  }
+}
+
+function Vram(){
+  var elemento = document.getElementById('Ram');
+  error = document.querySelector("#Ram + span.error");
+
+  if(elemento.checkValidity()){
+    error.innerHTML = "";
+    error.className = "error";
+    return true;
+  }
+  else{
+    if(elemento.validity.valueMissing){
+      muestra(elemento,"Ingrese la Memoria Ram");
+    }
+    if(elemento.validity.patternMismatch){
+      muestra(elemento,"Solo Acepta Letras Mayusculas");
+    }
+    return false;
+  }
+}
+
+function Vtpmemoria(){
+  var elemento = document.getElementById('TipoMemoria');
+  error = document.querySelector("#TipoMemoria + span.error");
+
+  if(elemento.checkValidity()){
+    error.innerHTML = "";
+    error.className = "error";
+    return true;
+  }
+  else{
+    if(elemento.validity.valueMissing){
+      muestra(elemento,"Ingrese el Tipo de Memoria");
+    }
+    return false;
+  }
+}
+
+function Vcontra(){
+  var elemento = document.getElementById('contraseña');
+  error = document.querySelector("#contraseña + span.error");
+
+  if(elemento.checkValidity()){
+    error.innerHTML = "";
+    error.className = "error";
+    return true;
+  }
+  else{
+    if(elemento.validity.valueMissing){
+      muestra(elemento,"Ingrese la Contraseña");
+    }
+    return false;
+  }
+}
+function VnEquipo(){
+  var elemento = document.getElementById('NEquipo');
+  error = document.querySelector("#NEquipo + span.error");
+
+  if(elemento.checkValidity()){
+    error.innerHTML = "";
+    error.className = "error";
+    return true;
+  }
+  else{
+    if(elemento.validity.valueMissing){
+      muestra(elemento,"Ingrese el Nombre del Equipo");
+    }
+    if(elemento.validity.patternMismatch){
+      muestra(elemento,"Solo Acepta Letras Mayusculas");
+    }
+    return false;
+  }
+}
+function VIP(){
+  var elemento = document.getElementById('IP');
+  error = document.querySelector("#IP + span.error");
+
+  if(elemento.checkValidity()){
+    error.innerHTML = "";
+    error.className = "error";
+    return true;
+  }
+  else{
+    if(elemento.validity.valueMissing){
+      muestra(elemento,"Ingrese la IP");
+    }
+    return false;
+  }
+}
+
+function VMac(){
+  var elemento = document.getElementById('Mac');
+  error = document.querySelector("#Mac + span.error");
+
+  if(elemento.checkValidity()){
+    error.innerHTML = "";
+    error.className = "error";
+    return true;
+  }
+  else{
+    if(elemento.validity.valueMissing){
+      muestra(elemento,"Ingrese la MAC");
     }
     return false;
   }
--- paginas/CPU'S.php
+++ paginas/CPU'S.php
@@ -1,10 +1,34 @@
+<?php
+if(isset($_POST['btnGuardar'])){
+
+    $NUsuario = $_POST['NUsuario'];
+    $APMaterno = $_POST['APMaterno'];
+    $APParteno = $_POST['APParteno'];
+    $Marca = $_POST['Marca'];
+    $Modelo = $_POST['Modelo'];
+    $numSerie = $_POST['numSerie'];
+    $numInventario = $_POST['numInventario'];
+    $So = $_POST['So'];
+    $Procesador = $_POST['Procesador'];
+    $DiscoDuro = $_POST['DiscoDuro'];
+    $Ram = $_POST['Ram'];
+    $TipoMemoria = $_POST['TipoMemoria'];
+    $Observaciones = $_POST['Observaciones'];
+    $contraseña = $_POST['contraseña'];
+    $NEquipo = $_POST['NEquipo'];
+    $IP = $_POST['IP'];
+    $Mac = $_POST['Mac'];
+    
+
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>CPU'S</title>
     <link rel="stylesheet" href="../css/pagina.css">
-    <script src="../js/validar.js"></script>
+    <script src="../js/validar.js" charset="utf-8"></script>
 </head>
 <header>
     <h1>Asistencia Tecnica y Mantenimiento</h1>
@@ -12,7 +36,7 @@
 <body>
     <main> 
         <section> 
-            <form action="#" id="formu">
+            <form action="#" method="post" id="formu" name="formu">
             <table>  
                 <caption id="titFormu">CAPTURA DE DATOS</caption>
             <tr>
@@ -29,68 +53,97 @@
                 </tr>
     
                 <td> 
-                    <label for="NUsuario" class="entrada">Nombre del Usuario:</label>
-                    <input type="text" id="NUsuario" class="entrada" pattern="[A-Z ]+">
-                    <br> <br>
-                    <span class="error"></span>
+
+                <label for="NUsuario" >Nombre del Usuario:
+                <input type="text" name="NUsuario" id="NUsuario" class="entrada" pattern="[A-Z ]+" required>
+
+                <span class="error"></span><br>
+                </label>
+
+                    <label for="APParteno">Apellido Paterno:
+                    <input type="text" id="APParteno" name="APParteno" class="entrada" pattern="[A-Z]+" required>
+                    <span class="error"></span><br>
+                    </label>
+
+                    <label for="APMaterno">Apellido Materno:
+                    <input type="text" id="APMaterno" name="APMaterno" class="entrada" pattern="[A-Z]+" required>
+                    <span class="error"></span><br>
+                    </label>
             
-                    <label for="APMaterno" class="entrada">Apellido Paterno:</label>
-                    <input type="text" id="APMaterno" class="entrada" pattern="[A-Z]+">
-                    
-                    <br><br>
-            
-                    <label for="APParteno" class="entrada">Apellido Materno:</label>
-                    <input type="text" id="APParteno" class="entrada" pattern="[A-Z]+">
-                    
-                    <br><br>
-            
-                    <label for="Marca" class="entrada">Marca:</label>
-                    <input type="text" id="Marca" class="entrada" pattern="[A-Z ]+"><br><br>
-                    
-                    <label for="Modelo" class="entrada">Modelo:</label>
-                    <input type="text" id="Modelo" class="entrada" pattern="[A-Z]+"><br><br>
-            
-                    <label for="numSerie" class="entrada">Numero de Serie:</label>
-                    <input type="number" id="numSerie" class="entrada" pattern="[0-9]+"><br><br>
-                    
-                    <label for="numInventario" class="entrada">Numero de Inventario:</label>
-                    <input type="number" id="numInventario" class="entrada" pattern="[0-9]+"><br><br>
+                    <label for="Marca">Marca:
+                    <input type="text" id="Marca" name="Marca" class="entrada" pattern="[A-Z ]+" required>
+                    <span class="error"></span><br>
+                    </label>
+
+                    <label for="Modelo">Modelo:
+                    <input type="text" id="Modelo" name="Modelo" class="entrada" pattern="[A-Z]+" required>
+                    <span class="error"></span><br>
+                    </label>
+
+                    <label for="numSerie">Numero de Serie:
+                    <input type="number" id="numSerie" name="numSerie" class="entrada" pattern="[0-9]+" required>
+                    <span class="error"></span><br>
+                    </label>
+
+                    <label for="numInventario">Numero de Inventario:
+                    <input type="number" id="numInventario" name="numInventario" class="entrada" pattern="[0-9]+" required>
+                    <span class="error"></span><br>
+                    </label>
                 </td>
 
                 <td> 
-                    <label for="So" class="entrada">So:</label>
-                    <input type="text" id="So"class="entrada"><br> <br>
+                    <label for="So">So:
+                    <input type="text" id="So" name="So" class="entrada" required>
+                    <span class="error"></span><br>
+                    </label>
             
-                    <label for="Procesador" class="entrada">Procesador:</label>
-                    <input type="text" id="Procesador" class="entrada"><br> <br>
-            
-                    <label for="DiscoDuro" class="entrada">Disco Duro:</label>
-                    <input type="text" id="DiscoDuro" class="entrada" pattern="[G,M,B,0-9 ]+"><br><br>
-            
-                    <label for="Ram" class="entrada">Memoria Ram:</label>
-                    <input type="text" id="Ram" pattern="[G,M,B,0-9 ]+" class="entrada"><br><br>
-                    
-                    <label for="TipoMemoria" class="entrada">Tipo de Memoria:</label>
-                    <input type="text" id="TipoMemoria" class="entrada"><br><br>
-            
-                    <label for="Observaciones" class="entrada">Observaciones:</label>
-                    <input type="text" id="Observaciones" class="CajonGrande"pattern="[A-Z]+"><br><br>
-                    
+                    <label for="Procesador">Procesador:
+                    <input type="text" id="Procesador" name="Procesador" class="entrada" required>
+                    <span class="error"></span><br>
+                    </label>
+
+                    <label for="DiscoDuro">Disco Duro:
+                    <input type="text" id="DiscoDuro" name="DiscoDuro" class="entrada" pattern="[G,M,B,0-9 ]+" required>
+                    <span class="error"></span><br>
+                    </label>
+
+                    <label for="Ram">Memoria Ram:
+                    <input type="text" id="Ram" name="Ram" class="entrada" pattern="[G,M,B,0-9 ]+" required>
+                    <span class="error"></span><br>
+                    </label>
+
+                    <label for="TipoMemoria">Tipo de Memoria:
+                    <input type="text" id="TipoMemoria"  name="TipoMemoria" class="entrada" required>
+                    <span class="error"></span><br>
+                    </label>
+
+                    <label for="Observaciones">Observaciones:
+                    <input type="text" id="Observaciones"  name="Observaciones" class="CajonGrande"pattern="[A-Z]+">
+                    <span class="error"></span><br>
+                    </label>
                 </td>
                 <td> 
-                    <label for="So" class="entrada">Contraseña:</label>
-                    <input type="password" id="So" class="entrada"><br> <br>
-            
-                    <label for="Procesador" class="entrada">Nombre del Equipo:</label>
-                    <input type="text" id="Procesador" class="entrada" pattern="[A-Z]+"><br> <br>
-            
-                    <label for="IP" class="entrada">IP:</label>
-                    <input type="text" id="IP" class="entrada" ><br><br>
-            
-                    <label for="Mac" class="entrada">Mac:</label>
-                    <input type="text" id="Mac" class="entrada"><br><br>
+                    <label for="contraseña">Contraseña:
+                    <input type="password" id="contraseña"  name="contraseña" class="entrada" required>
+                    <span class="error"></span><br>
+                    </label>
 
-                    <button id="btnGuardar" class="boton" type="submit">GUARDAR</button>
+                    <label for="NEquipo">Nombre del Equipo:
+                    <input type="text" id="NEquipo"  name="NEquipo" class="entrada" pattern="[A-Z]+" required>
+                    <span class="error"></span><br>
+                    </label>
+
+                    <label for="IP">IP:
+                    <input type="text" id="IP"  name="IP" class="entrada" required>
+                    <span class="error"></span><br>
+                    </label>
+
+                    <label for="Mac" >Mac:
+                    <input type="text" id="Mac"  name="Mac" class="entrada" required>
+                    <span class="error"></span><br>
+                    </label>
+
+                    <button id="btnGuardar" name="btnGuardar" class="boton" type="submit">GUARDAR</button>
                 </td>
                
             </table>
